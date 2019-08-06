a4dbcbd47eea60505d9f655e698387e813650af6
Gregory Roman <gregorymichaelroman@gmail.com>
2019-08-05 23:02:39 -0400

    Remove skrollr dependency.

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,1 @@
 require('../scss/main.scss');
-// const skrollr = require('skrollr');
-
-// skrollr.init({
-//   edgeStrategy: 'set',
-//   forceHeight: false,
-//   mobileCheck: function() {
-//     return false;
-//   }
-// });
